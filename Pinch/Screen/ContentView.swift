55114c7e967655abbf28c99cc37d3c2e219dd7f3
NowFloats <prathap.reddy@nowfloats.com>
2023-09-17 13:55:17 +0530

    Magnification gesture added

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -65,6 +65,26 @@ struct ContentView: View {
                                 }
                             }
                     )
+                // MARK: - 3. MAGNIFICATION GESTURE
+                    .gesture(
+                        MagnificationGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    if imageScale >= 1 && imageScale <= 5 {
+                                        imageScale = value
+                                    } else if imageScale > 5 {
+                                        imageScale = 5
+                                    }
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale > 5 {
+                                    imageScale = 5
+                                } else if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
             } //: ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
