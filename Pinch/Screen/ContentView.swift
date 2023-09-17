e81bb7c90c174bc78dab2581646488cd2f4d6c88
NowFloats <prathap.reddy@nowfloats.com>
2023-09-17 13:46:16 +0530

    Control interface added

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -80,6 +80,59 @@ struct ContentView: View {
                     .padding(.top, 30)
                 , alignment: .top
             )
+            // MARK: - CONTROLS
+            .overlay(
+                Group {
+                    HStack {
+                        // SCLAE DOWN
+                        
+                        Button {
+                            withAnimation(.spring()) {
+                                if imageScale > 1 {
+                                    imageScale -= 1
+                                    
+                                    if imageScale <= 1 {
+                                        resetImageState()
+                                    }
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "minus.magnifyingglass")
+                        }
+                        
+                        // RESET
+                        
+                        Button {
+                            resetImageState()
+                        } label: {
+                            ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
+                        }
+                        
+                        // SCALE UP
+                        
+                        Button {
+                            withAnimation(.spring()) {
+                                if imageScale < 5 {
+                                    imageScale += 1
+                                    
+                                    if imageScale > 5 {
+                                        imageScale = 5
+                                    }
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "plus.magnifyingglass")
+                        }
+                        
+                    } //: CONTROLS
+                    .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
+                }
+                .padding(.bottom, 30)
+                , alignment: .bottom
+            )
         } //: NAVIGATION
         .navigationViewStyle(.stack)
     }
