af12c6ad8bca82b647d3a205b588ebfc91b18e01
NowFloats <prathap.reddy@nowfloats.com>
2023-09-17 12:23:54 +0530

    App Icon added

---
+++ Pinch/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Prathap Reddy on 17/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - PROPERTY
+    
+    // MARK: - FUNCTION
+    
+    // MARK: - CONTENT
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Prathap Reddy on 17/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
